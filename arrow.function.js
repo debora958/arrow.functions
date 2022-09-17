7cf96b7a3bc2007f580855c2a5f06746c4c8e8e0
debora958 <109834177+debora958@users.noreply.github.com>
2022-09-17 11:52:55 -0300

    Add files via upload

---
+++ arrow.function.js
@@ -0,0 +1,20 @@
+//function tradicional sem parâmetros
+
+function nome(){
+    return "Débora"
+}
+console.log(nome())
+
+//function tradicional com parâmetros
+function soma(num, num2,){
+        soma = num + num2
+        return soma
+}
+console.log(soma(77,98))
+
+//arrow functions
+divisão = (num, num2) => num / num2 
+
+console.log(divisão(6,2))
+
+
